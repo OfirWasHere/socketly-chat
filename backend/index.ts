bb4239730fa9a5fd464b45f5cb74becf0d08f2cc
OfirWasHere <ofirdoron19@gmail.com>
2026-05-07 21:15:41 +0300

    moving to work on laptop

--- backend/index.ts
+++ backend/index.ts
@@ -8,8 +8,12 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hello world</h1>");
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("<h1>Hello world</h1>");
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(appConfig.port, () => {
