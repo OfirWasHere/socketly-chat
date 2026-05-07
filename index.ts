16b12dc66ef707709ef994e4788f8cbc779e1dcd
OfirWasHere <ofirdoron19@gmail.com>
2026-05-07 19:48:24 +0300

    add express

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 import { createServer } from "node:http";
 
 const app = express();
